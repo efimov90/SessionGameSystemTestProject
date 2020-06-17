6440e4862ad45ec60ad02bd107120422b30baf45
Sergey <efimov90@gmail.com>
2020-06-17 16:40:29 +0300

    Models

---
+++ SGSApi.ClientLibrary/Models/MissionResultModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGSApi.ClientLibrary.Models
+{
+    public class MissionResultModel
+    {
+        public int Id { get; set; }
+        public DateTime MissionStarted { get; set; }
+        public DateTime MissionEnded { get; set; }
+        public int MissionEndStatus { get; set; }
+    }
+}
